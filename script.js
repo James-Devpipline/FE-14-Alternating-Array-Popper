edea3325a285ad8dc2765a7fcf9e4096930622da
james <james@devpipeline.com>
2023-03-04 20:41:18 -0700

    a plan is now in place

---
+++ script.js
@@ -0,0 +1,17 @@
+/* 
+Alternating Array Popper
+  Using OOP, build a program that will take an array, and alternate between removing from the front and back every time your method is run.
+EX:
+const arr = [1, 2, 3, 4, 5]
+arrayPopper() => 1
+arrayPopper() => 5
+arrayPopper() => 2
+arrayPopper() => 4
+arrayPopper() => 3
+arrayPopper() => undefined
+*/
+
+function arrayPopper(arrayInput) {
+  count = 0;
+  // have count be 0 (falsey) to signify that it needs to take from the front, then have it be 1 (truthy) to signify it needs to take from the back
+}
